5726d5ff18bee3d0f652d38e441a907e7ae9af89
sergio <projectofinalrpa@gmail.com>
2026-04-22 00:20:34 +0200

    cleanup build files

--- build/coreLibrary_200/source/CMakeFiles/Newton.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "../../lib/libNewton.pdb"
-  "../../lib/libNewton.so"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang CXX)
-  include(CMakeFiles/Newton.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
